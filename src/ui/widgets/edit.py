cca912a8467743e985803730a352b95c16fbfcde
Anthony Nguyen <anthony.nguyen6336@gmail.com>
2026-07-17 11:31:26 -0500

    Changes to edit widget

--- src/ui/widgets/edit.py
+++ src/ui/widgets/edit.py
@@ -1,5 +1,9 @@
-from PySide6.QtWidgets import QWidget, QLineEdit, QVBoxLayout, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtCore import Qt, QUrl, QSize
+from PySide6.QtGui import QIcon
+from src.ui.components.file_upload import FileUpload
 
 
 class Edit(QWidget):
@@ -45,11 +49,52 @@ class Edit(QWidget):
 		self.setStyleSheet(layout_style)
 
 		layout = QVBoxLayout()
-		layout.setAlignment(Qt.AlignCenter)
+		layout.setAlignment(Qt.AlignTop)
+		layout.setContentsMargins(50, 50, 50, 0)
+		layout.setSpacing(20)
 
-		self.wip_label = QLabel("Work in Progress")
+		control_layout = QHBoxLayout()
+		control_layout.setAlignment(Qt.AlignCenter)
 
-		layout.addWidget(self.wip_label)
+		self.video_upload = FileUpload("Video", "Select a video to edit", set_read_only=True)
+
+		self.video_widget = QVideoWidget()
+		self.video_widget.setFixedSize(1024, 576)
+		self.video_widget.setStyleSheet("background-color: #ffffff;")
+
+		self.media_player = QMediaPlayer()
+		self.audio = QAudioOutput()
+
+		self.media_player.setAudioOutput(self.audio)
+		self.media_player.setVideoOutput(self.video_widget)
+
+		# Video Controls
+		self.play_button = QPushButton("Play")
+		self.play_button.setIcon(QIcon("data/assets/icons/play.png"))
+		self.play_button.setIconSize(QSize(24, 24))
+		self.pause_button = QPushButton("Pause")
+		self.pause_button.setIcon(QIcon("data/assets/icons/pause.png"))
+		self.pause_button.setIconSize(QSize(24, 24))
+
+		layout.addWidget(self.video_upload)
+		layout.addWidget(self.video_widget)
+		control_layout.addWidget(self.play_button)
+		control_layout.addWidget(self.pause_button)
+		layout.addLayout(control_layout)
 
 		self.setLayout(layout)
+
+		self.video_upload.path_changed.connect(self.load_video)
+		self.play_button.clicked.connect(self.play)
+		self.pause_button.clicked.connect(self.pause)
+
+	def load_video(self, path):
+		self.media_player.setSource(QUrl.fromLocalFile(path))
+		self.media_player.play()
+
+	def play(self):
+		self.media_player.play()
+
+	def pause(self):
+		self.media_player.pause()
 		
